7b392af3b3dd2c9db364bc65ccfea5a897b0ae22
imlavigne <matias.lavigne@gmail.com>
2023-06-06 15:04:57 -0300

    ejercicioN°1backv2

--- gastos.js
+++ gastos.js
@@ -15,8 +15,32 @@ const questions = [
     message: "¿precio?",
 
   },
- 
+  {
+    type: 'date',
+    name: 'date',
+    message: "¿cuando?",
+
+  },
+
 ];
+
+const filtrarPorGasto =[
+  {
+  
+  type: 'input',
+  name: 'gastos',
+  message: "Ingrese el gasto que desea filtrar",
+},
+]
+const filtrarPorFecha =[
+  {
+  
+  type: 'input',
+  name: 'fecha',
+  message: "Ingrese la fecha que desea filtrar",
+},
+]
+
 
 // inquirer.prompt(questions).then((answers) => {
 //   console.log(JSON.stringify(answers));
@@ -34,4 +58,37 @@ export const gastos =()=>{
 
 })
 }
-    
+
+export const filtroFecha =()=>{
+  return new Promise((resolve, reject)=>{
+      try{
+          inquirer.prompt(filtrarPorFecha)
+          .then(ans=>{resolve(ans)
+           })
+  }catch(error){
+      reject(error)
+  }
+
+})
+}
+ 
+
+export const filtroGasto =()=>{
+  return new Promise((resolve, reject)=>{
+      try{
+          inquirer.prompt(filtrarPorGasto)
+          .then(ans=>{resolve(ans)
+           })
+  }catch(error){
+      reject(error)
+  }
+
+})
+}
+
+
+export const gastosFiltrados =(filtro,lista)=>{
+  let filtrados = lista.filter(lista=>lista.price===filtro.gastos)
+  return(filtrados)
+  // console.log(filtrados)
+}
--- index.js
+++ index.js
@@ -1,17 +1,111 @@
-import { gastos } from "./gastos.js";
+import { gastos,  filtroFecha, filtroGasto, gastosFiltrados } from "./gastos.js";
 import { save, get } from "./readWriteGastos.js";
+import inquirer from "inquirer";
+
+
+
+
+
+
+
+ 
+
+ //run ()
+
+
+ const main = async()=>{
+    let promptCorriendo = true
+    while(promptCorriendo){
+        const opciones = await inquirer.prompt([
+            {
+              type: 'list',
+              name: 'choices',
+              message: "Selecciona una opcion",
+              choices:[
+                {value: 1, name: "Agregar nuevo gasto"},
+                {value: 2, name: "Obtener gastos"},
+                {value: 3, name: "filtrar gastos"},
+                {value: 99, name: "Exit"}
+              ]
+            },
+          ])
+    
+        switch(opciones.choices){
+            case 1:
+                await run();
+                break
+            case 2:
+                await obtenerGastos()
+                break
+            case 3:
+                await filterOptions()
+            case 99:
+                promptCorriendo = false
+                break
+            default:
+                promptCorriendo= false
+                break
+        }
+    }
+}
+
+main()
+
 
 const run = async ()=>{
     const gasto = await gastos();
-    
-
-    const gastoJson= await get("./gastos.json")
-
-    const newGasto = [...gastoJson,gasto]
-
-    save("./gastos.json",newGasto)
-
  
+   const gastoJson= await get("./gastos.json")
+    const newGasto = [...gastoJson,gasto]
+    save("./gastos.json",newGasto)
+ }
+ 
+
+const obtenerGastos = async ()=>{
+    
+    const gastos = await get("./gastos.json")
+    console.log(gastos)
+ }
+
+const filterOptions =async()=>{
+    const opciones = await inquirer.prompt([
+   
+    {
+        type: 'list',
+        name: 'choices',
+        message: "como desea realizar el filtro",
+        choices:[
+          {value: 1, name: "Por fecha"},
+          {value: 2, name: "Por gasto"},
+    
+          {value: 99, name: "Exit"}
+        ]
+      },
+    ])
+
+  switch(opciones.choices){
+      case 1:
+          await dateFilter();
+          break
+      case 2:
+          await priceFilter()
+          break
+        case 99:
+        break   
+
+
+  }
 }
 
-run ()
+const dateFilter = async ()=>{
+    const filtrado= await filtroFecha()
+    console.log(filtrado)
+}
+
+const priceFilter = async ()=>{
+    const filtrado= await filtroGasto()
+    const gastos = await get("./gastos.json")
+    const filtrarGastos= await gastosFiltrados(filtrado,gastos)
+    console.log(filtrarGastos)
+    
+}
